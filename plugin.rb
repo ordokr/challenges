51b839d236e8020c2f2c1a48495dd875ce4527fa
ordokr <tvail@wgu.edu>
2024-10-01 14:39:55 -0400

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,21 +1,44 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
+# name: discourse-challenges
+# about: Adds quests and challenges to Discourse
 # meta_topic_id: TODO
-# version: 0.0.1
-# authors: Discourse
-# url: TODO
+# version: 0.1
+# authors: Tim Vail
+# url: https://cafe.ordo.kr
 # required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :challenges_enabled
 
-module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
+module ::DiscourseChallenges
+  PLUGIN_NAME = "discourse-challenges"
+
+  class Engine < ::Rails::Engine
+    engine_name PLUGIN_NAME
+    isolate_namespace DiscourseChallenges
+  end
 end
 
-require_relative "lib/my_plugin_module/engine"
+# If you have additional engine code or need to isolate plugin logic in files
+# require_relative "lib/discourse_challenges/engine"
 
 after_initialize do
-  # Code which should run after Rails has finished booting
+  # This is where the core functionality of the plugin goes
+  class WeeklyPostChallenge
+    def self.track_progress(user)
+      # Count the number of posts the user made in the last 7 days
+      posts_count = Post
+                    .where(user_id: user.id)
+                    .where("created_at >= ?", 1.week.ago)
+                    .count
+
+      # Check if the user has made at least 10 posts
+      if posts_count >= 10
+        # Grant a badge if the challenge criteria are met
+        BadgeGranter.grant(user, Badge.find_by(name: 'Weekly Posting Champ'))
+      end
+    end
+  end
+
+  # Add any additional plugin initialization code here
 end
